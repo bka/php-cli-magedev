919085acca3c929c3837dea5f546040f7d2587db
Bernhard Leers <b.leers@gmx.de>
2018-01-23 13:21:04 +0100

    increased es version

--- src/Docker/Container/Repository/ElasticSearch.php
+++ src/Docker/Container/Repository/ElasticSearch.php
@@ -33,6 +33,6 @@ class ElasticSearch extends AbstractContainer
      */
     public function getImage()
     {
-        return 'elasticsearch:1.7.5';
+        return 'elasticsearch:5.4.3';
     }
 }
