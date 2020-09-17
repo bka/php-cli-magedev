4700c1203184b290b8d172f4235b1badf6d99b46
Bernhard Leers <bl@wamoco.de>
2020-09-17 09:36:08 +0200

    mysql 5.7

--- src/Docker/Image/Repository/Mysql.php
+++ src/Docker/Image/Repository/Mysql.php
@@ -35,6 +35,22 @@ class Mysql extends AbstractImage
      */
     public function configure()
     {
+        if ($this->config->optionExists('mysql_version')) {
+            $mysqlVersion = $this->config->get('mysql_version');
+        } else {
+            // default to current version
+            $mysqlVersion = "5.6";
+        }
+
+        if ($mysqlVersion == "5.6") {
+            $this->initMysqlDefault();
+        } else {
+            $this->initMysqlByVersion($mysqlVersion);
+        }
+    }
+
+    protected function initMysqlDefault()
+    {
         $this->name('mysql');
         $this->from('mysql:5.6');
 
@@ -50,4 +66,23 @@ class Mysql extends AbstractImage
         $this->addFile("var/Docker/mysql/my.cnf","/root/.my.cnf");
         $this->addFile("var/Docker/mysql/my.cnf","/var/www/.my.cnf");
     }
+
+    protected function initMysqlByVersion($version)
+    {
+        $this->name('mysql');
+        $this->from('mysql:' . $version);
+
+        $uid = getmyuid();
+        $this->run('usermod -u '.$uid.' mysql');
+
+        // addresses permission error, cannot bind socket
+        $this->run("chmod -R 777 /var/run/mysqld/");
+
+        $this->addFile("var/Docker/mysql/mysql-".$version.".cnf", "/etc/mysql/conf.d/z99-docker.cnf");
+        $this->run("chmod 644 /etc/mysql/conf.d/z99-docker.cnf");
+
+        $this->addFile("var/Docker/mysql/my.cnf","/root/.my.cnf");
+        $this->addFile("var/Docker/mysql/my.cnf","/var/www/.my.cnf");
+    }
+
 }
